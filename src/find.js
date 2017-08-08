fd90d2827d431c8086363efa97281c5be13861e3
jimmy2020 <jimmy002020@gmail.com>
2017-08-09 00:29:19 +0200

    fix long regex and chunk boundries closes #1, closes #2

--- src/find.js
+++ src/find.js
@@ -3,6 +3,10 @@ import fs from 'fs';
 // function to generates error as you see.
 const errUnit = errMsg => new Error(`\x1b[33m${errMsg}\x1b[0m`);
 
+// since all the validation happens after opening the file.
+const closeReturnError = (fd, errMsg, cb) => fs.close(fd, () => cb(errUnit(errMsg)));
+
+
 const cbMsg = 'Invalid callback function.';
 const arrOfRegMsg = 'Invalid array type';
 const arrOfRegEmptyMsg = 'Empty array of objects';
@@ -18,29 +22,38 @@ const isStr = str => typeof str === 'string';
 // check if input is regex
 const isReg = reg => typeof reg === 'object';
 
+
 /**
-  *
-  * @param {string} dir  - directory.
-  * @param {array} arrayOfReg
-  * @callback {error~object}
-  *
-  */
-const find = (dir, arrayOfReg, cb) => {
+ *
+ * @param {Object} opt - options
+ * @property {string} - path, directory
+ * @property {array} - requestز array contains the regex
+ * @property {string} - encoding, type of encoding used for reading
+ * @property {number} - rounds, chunk boundaries
+ *
+ * @callback {error~object}
+ * @property {boolean} - isFound
+ * @property {string|object} - reg
+ * @property {string} - match,
+ *
+ */
+const find = (opt, cb) => {
+  const opts = opt || {};
   // validate callback, else every error will be handled in callback error
   if (typeof cb !== 'function') throw errUnit(cbMsg);
   // validate file.
-  return fs.open(dir, 'r', (openErr, fd) => {
+  return fs.open(opts.path, 'r', (openErr, fd) => {
     if (openErr) return cb(openErr);
     // validate arrayOfReg
-    if (!(arrayOfReg instanceof Array)) return cb(errUnit(arrOfRegMsg));
+    if (!(opts.request instanceof Array)) return closeReturnError(fd, arrOfRegMsg, cb);
     // validate arrayOfReg length
-    const arrayLen = regLen(arrayOfReg);
-    if (arrayLen === 0) return cb(errUnit(arrOfRegEmptyMsg));
+    const arrayLen = regLen(opts.request);
+    if (arrayLen === 0) return closeReturnError(fd, arrOfRegEmptyMsg, cb);
     // validate the objects inside.
     for (let i = 0; i < arrayLen; i += 1) {
-      // validate regMsg in findd
-      if (!isReg(arrayOfReg[i]) && !isStr(arrayOfReg[i])) {
-        return cb(errUnit(regMsg + arrayOfReg[i]));
+      // validate regMsg in find
+      if (!isReg(opts.request[i]) && !isStr(opts.request[i])) {
+        return closeReturnError(fd, regMsg + opts.request[i], cb);
       }
     }
     // Everything is good ****************************************************
@@ -49,16 +62,37 @@ const find = (dir, arrayOfReg, cb) => {
     // init arry with default false.
     for (let i = 0; i < arrayLen; i += 1) isFlags[i] = null; // flags
     let isFoundAll = false;
-    const readStream = fs.createReadStream(dir, {
-      encoding: 'utf8',
+    const readStream = fs.createReadStream(opts.path, {
+      encoding: opts.encoding || 'utf8',
     });
-    readStream.on('error', readErr => cb(readErr));
+    readStream.on('error', readErr => closeReturnError(fd, readErr, cb));
+    const combinedChunkQueue = []; // chunck holder
+    let combinedChunk; // combined chunk
+    const maxRounds = opts.join || 3; // max rounds allowed to hold chunk
+    let currentRound = 0;
     readStream.on('data', (chunk) => {
+      // handling combined chunk index.
+      if (currentRound === maxRounds) {
+        // reset index
+        currentRound = 0;
+        // dump the first element in queue
+        combinedChunkQueue.shift();
+      } else {
+        currentRound += 1;
+        // add new chunck
+        combinedChunkQueue.push(chunk);
+      }
+      // flush combined chunk
+      combinedChunk = null;
+      // compose combined chunk
+      for (let j = 0; j < combinedChunkQueue.length; j += 1) combinedChunk += combinedChunkQueue[j];
+      // matching process
       for (let i = 0; i < arrayLen; i += 1) {
-        if (isFlags[i] === null) isFlags[i] = chunk.match(arrayOfReg[i]);
+        if (isFlags[i] === null) isFlags[i] = combinedChunk.match(opts.request[i]);
       }
       if (!isFlags.includes(null)) isFoundAll = true;
       if (isFoundAll) {
+        combinedChunk = null;
         readStream.destroy();
         readStream.emit('end');
       }
@@ -68,7 +102,7 @@ const find = (dir, arrayOfReg, cb) => {
       for (let i = 0; i < arrayLen; i += 1) {
         report[i] = {
           isFound: isFlags[i] !== null,
-          reg: arrayOfReg[i],
+          reg: opts.request[i],
           match: isFlags[i] && isFlags[i][0],
         };
       }
